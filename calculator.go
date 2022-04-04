168af386c87a9dbd332139a3c455110bd5eb04e2
HunKiat <hunkiat@gmail.com>
2022-04-04 17:20:10 +0800

    okay

--- calculator.go
+++ calculator.go
@@ -1,26 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func add(a, b int) int {
 	//Insert code here
-
+	return a + b
 }
 
 func subtract(a, b int) int {
 	//Insert code here
-
+	return a - b
 }
 
 func multiply(a, b int) int {
 	//Insert code here
-
+	return a * b
 }
 
 func divide(a, b int) int {
 	//Insert code here
 	//consider for b = 0
-
+	if b > 0 {
+		return a / b // returns the quotient only
+	}
+	return 0
 }
 
 func main() {
@@ -33,5 +38,18 @@ func main() {
 	fmt.Println("Enter an integer: ")
 	fmt.Scanln(&b)
 
-	//Insert code here
+	switch process {
+	case "add":
+		fmt.Printf("Add %d to %d gives %d", a, b, add(a, b))
+	case "sub":
+		fmt.Printf("Subtract %d from %d gives %d", b, a, subtract(a, b))
+	case "mul":
+		fmt.Printf("Multiply %d to %d gives %d", a, b, multiply(a, b))
+	case "div":
+		fmt.Printf("Divide %d over %d gives %d", a, b, divide(a, b))
+	default:
+		return
+	}
+	fmt.Println("")
+	main()
 }
